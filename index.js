34ca7a6c47308b154ebeda144412c03c3c7de359
Tuna <tunahanakgun@gmail.com>
2020-09-27 15:38:02 +0200

    rest mit fehler

--- index.js
+++ index.js
@@ -1,12 +1,70 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+
 const hostname = 'localhost' ;
 
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.all('/dishes', (req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+
+});
+
+
+app.get('/dishes', (req, res,next ) => {
+    res.end('Will send all the dishes to you!');
+});
+
+
+app.post('/dishes', (req,res,next) => {
+    res.end('Will add the dish: ' + req.body.name + 
+        'with details: ' + req.body.description);
+
+});
+
+app.put('/dishes', (req,res,next) => {
+    res.statusCode = 403;
+    res.end('Put operation not Supported on /dishes ');
+
+});
+
+app.delete('/dishes', (req,res,next) => {
+    res.end('Deleting all the dished!');
+
+});
+
+app.get('/dishes/:dishId' ,(req, res,next ) => {
+    res.end('Will send details of the dish: ' 
+    + req.params.dishId);
+
+});
+
+
+app.post('/dishes:dishId', (req,res,next) => {
+    res.statusCode = 403;
+    res.end('Post not supported on /dishes/ ');
+
+});
+
+app.put('/dishes:dishId', (req,res,next) => {
+    res.write('Updating the dish: ' + req.params.dishId + '\n');
+    res.end('Will update the dish:' + req.body.name +
+    'with details:' + req.body.description);
+});
+
+app.delete('/dishes:dishId', (req,res,next) => {
+    res.end('Deleting dish' + req.params.dishId);
+
+});
+
 
 app.use(express.static(__dirname+'/public'));
 /*
@@ -19,7 +77,7 @@ app.use((req, res, next) => {
 
 }
 );
-*/
+
 
 const server = http.createServer(app);
 
